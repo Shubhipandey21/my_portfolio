53942d599041b93ec4e810deeb5ed64ab37b1bb8
tusharrr-31 <singhtushar4411@gmail.com>
2023-09-13 22:40:59 +0530

    changes

--- portfolio_final/index.js
+++ portfolio_final/index.js
@@ -51,3 +51,98 @@ setTimeout(function(){
       $("#work").removeClass("animated fadeIn");
     },1000);
 },1500);
+const words = ['Software Developer', 'XR Developer', 'Web Developer'];
+let index = 0; // Current word index
+
+function typeWriter() {
+    const currentWord = words[index];
+    const wordElement = document.querySelector('.word');
+
+    let charIndex = 0;
+    const typeInterval = setInterval(() => {
+        if (charIndex < currentWord.length) {
+            wordElement.textContent += currentWord.charAt(charIndex);
+            charIndex++;
+        } else {
+            clearInterval(typeInterval);
+            setTimeout(eraseWord, 1000); // Wait for a second before erasing
+        }
+    }, 100); // Typing speed (adjust as needed)
+}
+
+function eraseWord() {
+    const wordElement = document.querySelector('.word');
+
+    let charIndex = wordElement.textContent.length;
+    const eraseInterval = setInterval(() => {
+        if (charIndex > 0) {
+            wordElement.textContent = wordElement.textContent.slice(0, charIndex - 1);
+            charIndex--;
+        } else {
+            clearInterval(eraseInterval);
+            index = (index + 1) % words.length; // Move to the next word
+            setTimeout(typeWriter, 500); // Wait for half a second before typing the next word
+        }
+    }, 50); // Erasing speed (adjust as needed)
+}
+
+// Start the typing animation
+typeWriter();
+const flavoursContainer = document.getElementById('flavoursContainer');
+const scrollSpeed = 1; // Adjust the scroll speed as needed
+
+let isDragging = false;
+let dragStartX = 0;
+let scrollStartX = 0;
+
+function startDrag(event) {
+  isDragging = true;
+  dragStartX = event.clientX || event.touches[0].clientX;
+  scrollStartX = flavoursContainer.scrollLeft;
+}
+
+function stopDrag() {
+  isDragging = false;
+}
+
+function performDrag(event) {
+  if (!isDragging) return;
+
+  const currentX = event.clientX || event.touches[0].clientX;
+  const dragDistance = currentX - dragStartX;
+
+  flavoursContainer.scrollLeft = scrollStartX - dragDistance;
+}
+
+flavoursContainer.addEventListener('mousedown', startDrag);
+flavoursContainer.addEventListener('touchstart', startDrag);
+
+window.addEventListener('mousemove', performDrag);
+window.addEventListener('touchmove', performDrag);
+
+window.addEventListener('mouseup', stopDrag);
+window.addEventListener('touchend', stopDrag);
+
+
+function scrollImages() {
+  if (!isDragging) {
+	const scrollLeft = flavoursContainer.scrollLeft;
+
+	// Check if we've reached the end, then reset to the beginning
+	if (scrollLeft >= flavoursContainer.scrollWidth - flavoursContainer.clientWidth) {
+	  flavoursContainer.scrollLeft = 0;
+	} else {
+	  // Scroll to the right by the scrollSpeed amount
+	  flavoursContainer.scrollLeft += scrollSpeed;
+	}
+  }
+}
+
+// Call the scrollImages function repeatedly for continuous scrolling
+const scrollInterval = setInterval(scrollImages, 25); // You can adjust the interval for the desired speed
+
+// Stop the scrolling when the page unloads (e.g., when navigating away)
+window.addEventListener('beforeunload', () => {
+  clearInterval(scrollInterval);
+});
+
